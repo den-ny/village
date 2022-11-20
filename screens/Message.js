f94855b07acdc61d9a5670f8d5c2ea0022f517be
Mitch Raznick <mraznick@gmail.com>
2022-11-19 19:09:52 -0500

    started message screen

---
+++ screens/Message.js
@@ -0,0 +1,121 @@
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  Button,
+  TouchableOpacity,
+  Pressable,
+} from "react-native";
+
+function Message({ navigation }) {
+  const [message, setMessage] = useState("");
+
+  return (
+    <View style={styles.container}>
+      {/* <Image style={styles.image} source={require("../assets/.png")} /> */}
+    <StatusBar style="auto" />
+
+      <View style={styles.navbar}>
+        <Button title="MENU BUtton"></Button>
+        <Text>The Village</Text>
+        <Button title="Pic"></Button>
+      </View>
+
+      <View style={styles.subheader}>
+        <Pressable style={styles.loginBtn}>
+          <Text
+            style={styles.loginText}
+            title="Back"
+            // CHANGE NAV TO INBOX
+            onPress={() => navigation.navigate("home")}
+          >
+            Back
+          </Text>
+        </Pressable>
+        <Text style={styles.header}>Messages</Text>
+      </View>
+
+      <View style={styles.sender}>
+        <Text>Sender Icon</Text>
+        <Text>Sender Name</Text>
+      </View>
+
+
+
+
+      {/* <TouchableOpacity>
+            <Text style={styles.forgot_button}>Forgot Password?</Text>
+          </TouchableOpacity> */}
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  image: {
+    marginBottom: 40,
+  },
+
+  inputView: {
+    backgroundColor: "#fff",
+    borderRadius: 30,
+    borderWidth: 1,
+    borderColor: "gray",
+    width: "70%",
+    height: 45,
+    marginBottom: 20,
+
+    alignItems: "center",
+  },
+
+  TextInput: {
+    height: 50,
+    flex: 1,
+    padding: 10,
+    marginLeft: 20,
+  },
+
+  forgot_button: {
+    height: 30,
+    marginBottom: 30,
+  },
+
+  loginBtn: {
+    width: "60%",
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
+    color: "#fff",
+    backgroundColor: "#978DF3",
+    marginTop: 100,
+  },
+
+  loginText: {
+    color: "#fff",
+    fontSize: 20,
+    fontWeight: "semi-bold",
+  },
+
+  header: {
+    fontSize: 36,
+    fontWeight: "bold",
+    marginBottom: 100,
+    color: "#978DF3",
+    fontFamily: "Times New Roman",
+  },
+});
+
+export default Message;
--- screens/index.js
+++ screens/index.js
@@ -3,11 +3,13 @@ import Upload from "./Upload";
 // import Capture from "./Capture";
 import Login from "./Login"
 import Register from "./Register.js";
+import Message from "./Message.js"
 
 export {
     Home,
     Upload,
     Login,
-    Register
+    Register,
+    Message
     // Capture
 }
