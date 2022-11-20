06a475a63fe1ecb81fcf521f7a129ee988dcc351
tom <tsames1@gmail.com>
2022-11-19 16:29:52 -0800

    Added connection to backend in Home.js

--- services/posts.js
+++ services/posts.js
@@ -1,33 +1,31 @@
-export const getPosts = async () => {
-  try {
-    const response = await fetch('https://ga-hackathon-backend.herokuapp.com/posts')
-    const json = await response.json()
-    return json
-  } catch (error) {
-    console.log(error)
-  }
-};
+// export const getPosts = async () => {
+//   try {
+//     const response = await fetch('https://ga-hackathon-backend.herokuapp.com/posts')
+//     const data = await response.json()
+//   } catch (error) {
+//     console.log(error)
+//   }
+// };
 
-export const getPostById = async (id) => {
-  try {
-    const response = await fetch(`https://ga-hackathon-backend.herokuapp.com/posts/${id}`)
-    const json = await response.json()
-    return json
-  } catch (error) {
-    console.log(error)
-  }
-};
+// export const getPostById = async (id) => {
+//   try {
+//     const response = await fetch(`https://ga-hackathon-backend.herokuapp.com/posts/${id}`)
+//     return await response.json()
+//   } catch (error) {
+//     console.log(error)
+//   }
+// };
 
-export const createPost = async (post) => {
-  try {
-    await fetch(`https://ga-hackathon-backend.herokuapp.com/posts/`, {
-      method: "post",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(post)
-    })
-  } catch (error) {
-    console.log(error)
-  }
-};
+// export const createPost = async (post) => {
+//   try {
+//     await fetch(`https://ga-hackathon-backend.herokuapp.com/posts/`, {
+//       method: "post",
+//       headers: {
+//         "Content-Type": "application/json"
+//       },
+//       body: JSON.stringify(post)
+//     })
+//   } catch (error) {
+//     console.log(error)
+//   }
+// };
