3e0f0f9002382d50ca4a4ebf4d6150a4de2232b4
Mitch Raznick <mraznick@gmail.com>
2022-11-20 22:27:38 -0500

    inbox styles

--- App.js
+++ App.js
@@ -25,11 +25,11 @@ export default function App() {
       <Stack.Navigator screenOptions={{ headerShown: true }}>
         {/* <Stack.Screen name="capture" component={Capture} /> */}
         {/* <Stack.Screen name="SignUp" component={Register} /> */}
-        {/* <Stack.Screen name="Inbox" component={Inbox} /> */}
+        <Stack.Screen name="Inbox" component={Inbox} />
         {/* <Stack.Screen name="Message" component={Message} /> */}
         
         {/* <Stack.Screen name="AccountInfo" component={AccountInfo} /> */}
-        <Stack.Screen name="ProductInfo" component={ProductInfo} />
+        {/* <Stack.Screen name="ProductInfo" component={ProductInfo} /> */}
         
         {/* <Stack.Screen name="Posting" component={Posting} />
         <Stack.Screen name="Upload" component={Upload} /> */}
